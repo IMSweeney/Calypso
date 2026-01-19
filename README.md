84ee920251cb0ce8c76a291e804172e1f2eb8896
Ian Sweeney <Ian Sweeney>
2026-01-18 22:26:20 -0800

    save calibration in flash

---
+++ README.md
@@ -0,0 +1,6 @@
+TODO
+
+- [X] Save calibration to memory between power cycles
+- [ ] Simple level sensor along one axis
+- [ ] Export data over BLE
+- [ ] OTA code updates
--- calypso/src/bin/main.rs
+++ calypso/src/bin/main.rs
@@ -17,8 +17,11 @@ use esp_hal::i2c::master::I2c;
 use esp_println as _;
 use esp_radio::ble::controller::BleConnector;
 use trouble_host::prelude::*;
+use esp_bootloader_esp_idf::partitions;
+use esp_storage::FlashStorage;
+use embedded_storage::{ReadStorage, Storage};
 
-use bno055::BNO055OperationMode;
+use bno055::{BNO055OperationMode, BNO055Calibration, BNO055_CALIB_SIZE};
 use bno055::Bno055;
 
 #[panic_handler]
@@ -30,6 +33,8 @@ extern crate alloc;
 
 const CONNECTIONS_MAX: usize = 1;
 const L2CAP_CHANNELS_MAX: usize = 1;
+
+const BNO055_CALIBRATION_ADDR: u32 = 0x00;
 
 // This creates a default app-descriptor required by the esp-idf bootloader.
 // For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
@@ -73,7 +78,7 @@ async fn main(spawner: Spawner) -> ! {
     info!("Setting up i2c");
     // pinout for esp32 pico
     // https://learn.adafruit.com/assets/112309
-    let mut i2c = I2c::new(peripherals.I2C0, esp_hal::i2c::master::Config::default()).unwrap()
+    let i2c = I2c::new(peripherals.I2C0, esp_hal::i2c::master::Config::default()).unwrap()
     .with_sda(peripherals.GPIO22).with_scl(peripherals.GPIO19);
     info!("Done setting up i2c");
 
@@ -85,8 +90,37 @@ async fn main(spawner: Spawner) -> ! {
     info!("Bno055 initialized");
     bno055.set_mode(BNO055OperationMode::NDOF, &mut delay).unwrap();
 
-    // info!("Waiting for BNO055 to calibrate");
-    // while !bno055.is_fully_calibrated().unwrap() {}
+    // Set up nvs to store calibration data
+    let mut flash = FlashStorage::new(peripherals.FLASH);
+    info!("Flash size = {}", flash.capacity());
+
+    let mut pt_mem = [0u8; partitions::PARTITION_TABLE_MAX_LEN];
+    let pt = partitions::read_partition_table(&mut flash, &mut pt_mem).unwrap();
+
+    let nvs = pt
+        .find_partition(partitions::PartitionType::Data(
+            partitions::DataPartitionSubType::Nvs,
+        ))
+        .unwrap()
+        .unwrap();
+    let mut nvs_partition = nvs.as_embedded_storage(&mut flash);
+
+    let mut bno055_calibration: [u8; BNO055_CALIB_SIZE] = [0; BNO055_CALIB_SIZE];
+    nvs_partition
+        .read(BNO055_CALIBRATION_ADDR, &mut bno055_calibration)
+        .unwrap();
+
+    // Calibration
+    let mut calib = BNO055Calibration::from_buf(&bno055_calibration);
+    bno055.set_calibration_profile(calib, &mut delay).unwrap();
+    if !bno055.is_fully_calibrated().unwrap() {
+        info!("Waiting for BNO055 to calibrate");
+        while !bno055.is_fully_calibrated().unwrap() {}
+        calib = bno055.calibration_profile(&mut delay).unwrap();
+        nvs_partition
+        .write(BNO055_CALIBRATION_ADDR, &calib.as_bytes())
+        .unwrap();
+    }
 
     info!("BNO055 calibrated");
 
@@ -97,7 +131,7 @@ async fn main(spawner: Spawner) -> ! {
                 "Quaternion: w={} x={} y={} z={}",
                 quat.s, quat.v.x, quat.v.y, quat.v.z
             ),
-            Err(e) => warn!("BNO055 quaternion read error"),
+            Err(_e) => warn!("BNO055 quaternion read error"),
         }
         Timer::after(Duration::from_secs(1)).await;
     }
